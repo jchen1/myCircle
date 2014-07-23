32e61d3faff57e2057d87d7ef23d8b686c33ba0b
Jeff Chen <jeffrey@cmu.edu>
2014-07-23 15:19:08 -0400

    (very basic) notification support

--- app/src/main/java/f/myCircle/background/UkService.java
+++ app/src/main/java/f/myCircle/background/UkService.java
@@ -1,17 +1,24 @@
 package f.myCircle.background;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 
+import com.f.myCircle.R;
+
 import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import f.myCircle.activities.MainActivity;
 import f.myCircle.models.DatabaseManager;
 import f.myCircle.models.ContactModel;
 
@@ -34,12 +41,43 @@ public class UkService extends Service {
         scheduler.scheduleAtFixedRate(new Runnable() {
             public void run() {
                 for (ContactModel contact : db.getAddedContacts()) {
-                    if ((new Date()).getTime() > contact.getLastContacted().getTime()+contact.getTtk().getTime()){
+                    long time = new Date().getTime();
+                    long expire = contact.getLastContacted().getTime() + contact.getTtk().getTime();
+                    if (time > expire){
                         db.deleteContact(contact);
+                    }
+                    else if (time + (1000l * 60l * 60 * 24) > expire) { // one day for testing
+                        Notification.Builder mBuilder =
+                                new Notification.Builder(getApplicationContext())
+                                        .setSmallIcon(R.drawable.logo)
+                                        .setContentTitle("My notification")
+                                        .setContentText("Hello World!");
+// Creates an explicit intent for an Activity in your app
+                        Intent resultIntent = new Intent(getApplicationContext(), MainActivity.class);
+
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+                        TaskStackBuilder stackBuilder = TaskStackBuilder.create(getApplicationContext());
+// Adds the back stack for the Intent (but not the Intent itself)
+                        stackBuilder.addParentStack(MainActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+                        stackBuilder.addNextIntent(resultIntent);
+                        PendingIntent resultPendingIntent =
+                                stackBuilder.getPendingIntent(
+                                        0,
+                                        PendingIntent.FLAG_UPDATE_CURRENT
+                                );
+                        mBuilder.setContentIntent(resultPendingIntent);
+                        NotificationManager mNotificationManager =
+                                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+                        mNotificationManager.notify(0, mBuilder.build());
                     }
                 }
             }
-        }, 0, 1, TimeUnit.DAYS);
+        }, 0, 1, TimeUnit.MINUTES);
 
         return Service.START_NOT_STICKY;
     }
